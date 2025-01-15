673bb08a336dae98c258ad835726fc66404ea691
Bresnier moreno <bressmoreno20@gmail.com>
2025-01-15 11:34:37 -0400

    Initial Commit

---
+++ EarthQuakesApp/EarthQuakesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakesAppApp.swift
+//  EarthQuakesApp
+//
+//  Created by Bresnier Moreno on 15/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
